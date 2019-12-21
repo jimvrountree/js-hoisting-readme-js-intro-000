6ad16f64a8d9874944f5b1c413ac5f4f56075ed6
Jim Rountree <jimvrountree@gmail.com>
2019-12-21 22:35:07 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,34 @@
-function callMe() {
+
+  var callMe = function callMe {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+
 }
 
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  var crazy = function (){
+  // fix the code in here:
+
+
+var thisIsCrazy = function () {
     console.log("hey!!!")
   }
-}
-
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
+  thisIsCrazy();
+    }
+    crazy()
+var sayMyName = function(); {
+  var name;
+  name = "Cricky";
 
   function sayMy() {
+  var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
-}
+  sayMy();
+  }
+  sayMyName()()
